3f273cc3d6018df559bcc02cf0d219c12616ce5e
Макар Бадерко <makarbaderko@gmail.com>
2020-09-22 18:15:12 +0300

    Added pasword system

--- Assets/Hacker.cs
+++ Assets/Hacker.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,6 +7,7 @@ public class Hacker : MonoBehaviour
 {
     //Game State
     private int level;
+    private string password;
 
     enum Screen { MainMenu, Password, Win };
     Screen currentScreen = Screen.MainMenu;
@@ -28,8 +30,25 @@ public class Hacker : MonoBehaviour
         {
             Terminal.ClearScreen();
             ShowMainMenu("Makar");
-        } else if (currentScreen == Screen.MainMenu) //If tou are in the menu, you can select the level
+        }
+        else if (currentScreen == Screen.MainMenu)
+        { //If tou are in the menu, you can select the level
             RunMainMenu(input);
+        } else if (currentScreen == Screen.Password)
+        {
+            CheckPassword(input);
+        }
+    }
+
+    private void CheckPassword(string input)
+    {
+        if (input == password)
+        {
+            Terminal.WriteLine("WELL DONE!");
+        } else
+        {
+            Terminal.WriteLine("Wrong password, sorry!");
+        }
     }
 
     private void RunMainMenu(string input)
@@ -37,17 +56,19 @@ public class Hacker : MonoBehaviour
         if (input == "1")
         {
             level = 1;
+            password = "donkey";
             StartGame();
         }
         else if (input == "2")
         {
             level = 2;
+            password = "combobulate";
             StartGame();
-
         }
         else if (input == "3")
         {
             level = 3;
+            password = "astronaut";
             StartGame();
         }
         else if (input == "007")
